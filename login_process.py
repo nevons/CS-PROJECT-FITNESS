b576f69b60cc20869fcd7d85a0dbfffc9a76dd0e
nevons <tyaginavanshpe@gmail.com>
2024-06-09 18:08:36 +0300

    1.21

--- login_process.py
+++ login_process.py
@@ -1,11 +1,9 @@
 import mysql.connector as m
-import pyautogui
-import openai
 import webbrowser
 import time
 
 #db config
-'''mydb=m.connect(passwd="heilhitler666",host="localhost",user="root",auth_plugin="mysql_native_password")
+mydb=m.connect(passwd="heilhitler666",host="localhost",user="root",auth_plugin="mysql_native_password")
 mycursor=mydb.cursor()
 
 mycursor.execute("show databases;")
@@ -20,12 +18,6 @@ try:
 except:
     print("Oops! An error occured...")
 
-def acc_code_maker():
-    tb_ls=mycursor.execute("show tables;")
-    code_ls=[]
-    for i in tb_ls:
-        mycursor.execute("select code from %s",i)'''
-
 #key maker for openai
 conf=input('Do you have an OpenAi account(y/n)?: ')
 if conf=='y'or'Y':
@@ -37,14 +29,18 @@ if conf=='y'or'Y':
         api_key=input('enter your API key: ')
 
 #account manager
-'''print("Log in to existing account or create a new one.....")
+print("Log in to existing account or create a new one.....")
 c1=int(input("Enter 1:Log in or 2:Create new account: "))
 
 if c1==2:
-    acc_name=input("Enter your name: ")
+    acc_name=input("Enter your name(no spaces in between): ")
     acc_age=int(input("Enter your age: "))
-    acc_code=acc_code_maker()
-    mycursor.execute("create table user_%s () ")
+    acc_api_key=api_key
 
-mydb.close()'''
+    acc_tuple=tuple(acc_api_key,acc_name,acc_age)
 
+    mycursor.execute("create table user_%s %s ",(acc_name,acc_tuple))
+
+
+
+mydb.close()
