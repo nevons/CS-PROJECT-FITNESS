049d69612b3ddcd63a4d0e3faaca6873c44d6c09
nevons <tyaginavanshpe@gmail.com>
2024-06-09 17:56:30 +0300

    1.13

--- main_prog.py
+++ main_prog.py
@@ -2,9 +2,10 @@ import mysql.connector as m
 import pyautogui
 import openai
 import webbrowser
+import time
 
 #db config
-mydb=m.connect(passwd="heilhitler666",host="localhost",user="root",auth_plugin="mysql_native_password")
+'''mydb=m.connect(passwd="heilhitler666",host="localhost",user="root",auth_plugin="mysql_native_password")
 mycursor=mydb.cursor()
 
 mycursor.execute("show databases;")
@@ -23,11 +24,20 @@ def acc_code_maker():
     tb_ls=mycursor.execute("show tables;")
     code_ls=[]
     for i in tb_ls:
-        mycursor.execute("select code from %s",i)
+        mycursor.execute("select code from %s",i)'''
 
+#key maker for openai
+conf=input('Do you have an OpenAi account(y/n)?: ')
+if conf=='y'or'Y':
+    conf_1=input('Do you have an API key(y/n)?: ')
+    if conf_1=='n'or'N':
+        print('Having an API key is essential to use NutriPal.\n Opening the OpenAI website..... \nIt may ask you to login.\n After you are done making a key, enter it in the prompt below...')
+        time.sleep(2.5)
+        webbrowser.open('https://platform.openai.com/settings/profile?tab=api-keys')
+        api_key=input('enter your API key: ')
 
 #account manager
-print("Log in to existing account or create a new one............")
+'''print("Log in to existing account or create a new one.....")
 c1=int(input("Enter 1:Log in or 2:Create new account: "))
 
 if c1==2:
@@ -36,12 +46,5 @@ if c1==2:
     acc_code=acc_code_maker()
     mycursor.execute("create table user_%s () ")
 
-mydb.close()
+mydb.close()'''
 
-#key maker for openai
-conf=input('Do you have an OpenAi account(y/n)?: ')
-if conf=='y'or'Y':
-    conf_1=input('Do you have an API key(y/n)?: ')
-    if conf_1=='n'or'N':
-        print('Having an API key is essential to ')
-        webbrowser.open('https://platform.openai.com/login')
